69ba72cf3ebabe613df453494c6ef4d390274f7a
Eric Tobin <eatobin@gmail.com>
2016-10-27 14:32:21 -0500

    main success

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,21 @@
+import scala.concurrent.stm._
+
 object Main {
 
-  //  val tvBorrowers: Ref[Option[List[Borrower]]] = Option[List[Borrower]]()
-  //  val tvBooks: Ref[List[Book]] = Ref(List())
+  val tvBorrowers: Ref[List[Borrower]] = Ref(List())
+  val tvBooks: Ref[List[Book]] = Ref(List())
 
   def main(args: Array[String]): Unit = {
-    //
-    //    atomic { implicit txn =>
-    //      tvBorrowers.transform(l => Library.addBorrower(Borrower("Jim", 3), l))
-    ////      karen.transform(i => add30(i))
-    ////      println("karen2: " + karen.get)
-    //    }
-    //
-  }
 
-  //
-  //    Library.addBorrower(Borrower("Jim", 3), tvBorrowers)
-  //    Library.addBorrower(Borrower("Sue", 3), tvBorrowers)
-  //    Library.addBook(Book("War And Peace", "Tolstoy", None), tvBooks)
-  //    Library.addBook(Book("Great Expectations", "Dickens", None), tvBooks)
-  //    println("\nJust created new library")
+    atomic { implicit txn =>
+      tvBorrowers.transform(Library.addBorrower(Borrower("Jim", 3), _))
+      tvBorrowers.transform(Library.addBorrower(Borrower("Sue", 3), _))
+      tvBooks.transform(Library.addBook(Book("War And Peace", "Tolstoy", None), _))
+      tvBooks.transform(Library.addBook(Book("Great Expectations", "Dickens", None), _))
+      println("\nJust created new library")
+      println(Library.statusToString(tvBooks.get, tvBorrowers.get))
+    }
+
+  }
 
 }
