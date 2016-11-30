e3bb5098dabbc7c2e185c8f18dc07725b78df11a
Eric Tobin <eatobin@gmail.com>
2016-11-30 14:21:54 -0600

    progress

--- src/main/scala/TestIt.scala
+++ src/main/scala/TestIt.scala
@@ -2,35 +2,20 @@ import java.io.{BufferedWriter, FileWriter}
 
 import spray.json._
 
-//
-//object TestIt extends App with DefaultJsonProtocol with NullOptions{
-//
-//  case class Borrower(name: String, maxBooks: Int)
-//  case class Book(title: String, author: String, borrower: Borrower)
-//
-//  implicit val bookFormat = jsonFormat3(Book)
-//  implicit val borrowerFormat = jsonFormat2(Borrower)
-//  val book = Book("Title1", "Author1", Borrower("Borrower1", 1))
-//  val w = new BufferedWriter(new FileWriter("output.json"))
-//  w.write(book.toJson.prettyPrint)
-//  w.close()
-//}
-
-
 object TestIt extends App with DefaultJsonProtocol with NullOptions {
 
-  case class Borrower(title: String, maxBooks: Int)
+  case class Borrower(name: String, maxBooks: Int)
 
-  case class Book(name: String, author: String, borrower: Borrower)
+  case class Book(title: String, author: String, borrower: Borrower)
 
   implicit val borrowerFormat = jsonFormat2(Borrower)
   implicit val bookFormat = jsonFormat3(Book)
 
-  val book = Book("Title1", "Author1", Borrower("Borrower1", 111)).toJson
+  val book = Book("Title1", "Author1", Borrower("Borrower1", 1)).toJson
 
   val w = new BufferedWriter(new FileWriter("output.json"))
   w.write(book.toJson.prettyPrint)
   w.close()
+
 }
 
-//val json = Item("mary", Value(2)).toJson
