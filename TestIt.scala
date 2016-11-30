d8ed61c6be2d91627b83f030561bfc5bb39d6fbd
Eric Tobin <eatobin@gmail.com>
2016-11-30 15:48:19 -0600

    progress

---
+++ TestIt.scala
@@ -0,0 +1,36 @@
+//import java.io.{BufferedWriter, FileWriter}
+
+import spray.json._
+
+//object TestIt extends App with DefaultJsonProtocol with NullOptions {
+//
+//  case class Borrower(name: String, maxBooks: Int)
+//
+//  case class Book(title: String, author: String, borrower: Option[Borrower])
+//
+//  implicit val borrowerFormat = jsonFormat2(Borrower)
+//  implicit val bookFormat = jsonFormat3(Book)
+//
+//  val book = Book("Title1", "Author1", Some(Borrower("Borrower1", 1))).toJson
+//  //  val book = Book("Title1", "Author1", None).toJson
+//
+//  val w = new BufferedWriter(new FileWriter("output.txt"))
+//  w.write(book.toJson.toString)
+//  w.close()
+//
+//}
+
+case class Value(amt: Int)
+
+case class Item(name: String, count: Value)
+
+object MyJsonProtocol extends DefaultJsonProtocol {
+  implicit val valueFormat = jsonFormat1(Value)
+  implicit val itemFormat = jsonFormat2(Item)
+}
+
+import MyJsonProtocol._
+
+val json = Item("mary", Value(2)).toJson
+
+println(json)
