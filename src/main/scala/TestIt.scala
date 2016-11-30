47ba349999b107883b0784be5a058084b71ecaa4
Eric Tobin <eatobin@gmail.com>
2016-11-30 13:57:48 -0600

    progress

--- src/main/scala/TestIt.scala
+++ src/main/scala/TestIt.scala
@@ -2,13 +2,35 @@ import java.io.{BufferedWriter, FileWriter}
 
 import spray.json._
 
-object TestIt extends App with DefaultJsonProtocol {
+//
+//object TestIt extends App with DefaultJsonProtocol with NullOptions{
+//
+//  case class Borrower(name: String, maxBooks: Int)
+//  case class Book(title: String, author: String, borrower: Borrower)
+//
+//  implicit val bookFormat = jsonFormat3(Book)
+//  implicit val borrowerFormat = jsonFormat2(Borrower)
+//  val book = Book("Title1", "Author1", Borrower("Borrower1", 1))
+//  val w = new BufferedWriter(new FileWriter("output.json"))
+//  w.write(book.toJson.prettyPrint)
+//  w.close()
+//}
 
-  case class Foo(name: String, x: String, z: Int)
 
-  implicit val fooFormat = jsonFormat3(Foo.apply)
-  val foo = Foo("xxx", "jj", 60)
+object TestIt extends App with DefaultJsonProtocol with NullOptions {
+
+  case class Value(amt: Int)
+
+  case class Item(name: String, count: Value)
+
+  implicit val valueFormat = jsonFormat1(Value)
+  implicit val itemFormat = jsonFormat2(Item)
+
+  val json = Item("mary", Value(2)).toJson
+
   val w = new BufferedWriter(new FileWriter("output.json"))
-  w.write(foo.toJson.prettyPrint)
+  w.write(json.toJson.prettyPrint)
   w.close()
 }
+
+//val json = Item("mary", Value(2)).toJson
