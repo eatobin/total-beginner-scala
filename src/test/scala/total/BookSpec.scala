3a32b548a79327487bdb725ff7e3933381b07ad9
Eric Tobin <eatobin@gmail.com>
2017-03-30 14:35:42 -0500

    elixir same progress

--- src/test/scala/total/BookSpec.scala
+++ src/test/scala/total/BookSpec.scala
@@ -6,23 +6,15 @@ class BookSpec extends FlatSpec with Matchers {
 
   val br2: Borrower = Borrower.makeBorrower("Borrower2", 2)
   val bk1: Book = Book.makeBook("Title1", "Author1")
-  //  val bk1 = Book("Title1", "Author1", Some(Borrower("Borrower1", 1)))
-  //  val bk2 = Book("Title2", "Author2", None)
-  //  val br2 = Some(Borrower("Borrower2", 2))
   val bk2: Book = Book.setBorrower(Some(br2), bk1)
-  val bk3 = Book("Title2", "Author2", Some(Borrower("Borrower2", 2)))
 
   "A Book" should "create itself properly" in {
     Book.getTitle(bk1) should be("Title1")
     Book.getAuthor(bk1) should be("Author1")
+    Book.getBorrower(bk1) should be(None)
     Book.getBorrower(bk2) should be(Some(br2))
-    //    Book.getBorrower(bk2) should be(None)
   }
 
-  //  it should "set a new Borrower" in {
-  //    Book.setBorrower(br2, bk2) should be(bk3)
-  //  }
-  //
   //  it should "return a string \"Title1 by Author1; Checked out to Borrower1\"" in {
   //    Book.bookToString(bk1) should be("Title1 by Author1; Checked out to Borrower1")
   //  }
