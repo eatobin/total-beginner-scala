dfd3f215983f08d3f1473281f57a4a6dbd24ba99
Eric Tobin <eatobin@gmail.com>
2021-02-28 16:16:29 -0600

    book json string

--- src/main/scala/total/Book.scala
+++ src/main/scala/total/Book.scala
@@ -3,6 +3,7 @@ package total
 import io.circe.Error
 import io.circe.generic.auto._
 import io.circe.parser._
+import io.circe.syntax.EncoderOps
 
 case class Book(title: String, author: String, borrower: Option[Borrower] = None)
 
@@ -28,4 +29,6 @@ object Book {
 
   def bookJsonStringToBook(bookString: String): Either[Error, Book] = decode[Book](bookString)
 
+  def bookToJsonString(bk: Book): JsonString =
+    bk.asJson.noSpaces
 }
