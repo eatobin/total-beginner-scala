47d43c5ecb3f2a10d486e3ccaefd3211deb9a20c
Eric Tobin <eatobin@gmail.com>
2021-03-23 10:45:55 -0500

    borrower progress

--- src/test/scala/total/BorrowerSpec.scala
+++ src/test/scala/total/BorrowerSpec.scala
@@ -27,25 +27,37 @@ class BorrowerSpec extends AnyFlatSpec {
       case Left(e) => assert(e.toString == "")
       case Right(br) => assert(br.maxBooks == 1)
     }
-
-
-    //      assert(Borrower.getName(br1) == "Borrower1")
-    //      assert(br1.name == "Borrower1")
-    //      assert(Borrower.getMaxBooks(br1) == 1)
-    //      assert(br1.maxBooks == 1)
   }
-  //
-  //  it should "set a new name and maxBooks" in {
-  //    assert(Borrower.setName("Borrower1")(Borrower("Jack", 1)) == br1)
-  //    assert(Borrower.setMaxBooks(1)(Borrower("Borrower1", 11)) == br1)
-  //  }
-  //
-  //  it should "return a string \"Borrower1 (1 books)\"" in {
-  //    assert(Borrower.toString(br1) == "Borrower1 (1 books)")
-  //  }
-  //
-  //  it should "turn a Borrower into a JSON string" in {
-  //    assert(Borrower.borrowerToJsonString(br1) == jsonStringBr)
-  //  }
 
+  it should "NOT create itself properly - brWonky" in {
+    brWonky match {
+      case Left(e) => assert(e.toString == "DecodingFailure(Attempt to decode value on failed cursor, List(DownField(name)))")
+      case Right(br) => assert(Borrower.getName(br) == "")
+    }
+  }
+
+  it should "set a new name and maxBooks" in {
+    br1 match {
+      case Left(e) => assert(e.toString == "")
+      case Right(br) => assert(Borrower.setName("Borrower1")(Borrower("Jack", 1)) == br)
+    }
+    br1 match {
+      case Left(e) => assert(e.toString == "")
+      case Right(br) => assert(Borrower.setMaxBooks(1)(Borrower("Borrower1", 11)) == br)
+    }
+  }
+
+  it should "return a string \"Borrower1 (1 books)\"" in {
+    br1 match {
+      case Left(e) => assert(e.toString == "")
+      case Right(br) => assert(Borrower.toString(br) == "Borrower1 (1 books)")
+    }
+  }
+
+  it should "turn a Borrower into a JSON string" in {
+    br1 match {
+      case Left(e) => assert(e.toString == "")
+      case Right(br) => assert(Borrower.borrowerToJsonString(br) == jsonStringBr)
+    }
+  }
 }
