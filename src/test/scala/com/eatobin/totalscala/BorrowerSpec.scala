b2db12200f1af172e6315516ce96fb5c49897900
Eric Tobin <eatobin@gmail.com>
2022-07-29 16:48:14 -0500

    all changed!!!

--- src/test/scala/com/eatobin/totalscala/BorrowerSpec.scala
+++ src/test/scala/com/eatobin/totalscala/BorrowerSpec.scala
@@ -2,18 +2,19 @@ package com.eatobin.totalscala
 
 import io.circe.Error
 import org.scalatest.flatspec.AnyFlatSpec
+import com.eatobin.totalscala.Borrower._
 
 class BorrowerSpec extends AnyFlatSpec {
 
   private val jsonStringBr: JsonString = "{\"name\":\"Borrower1\",\"maxBooks\":1}"
   private val wonkyBr: JsonString = "{\"wonky\":\"Borrower1\",\"maxBooks\":1}"
-  private val br1: Either[Error, Borrower] = Borrower.jsonStringToBorrower(jsonStringBr)
-  private val brWonky: Either[Error, Borrower] = Borrower.jsonStringToBorrower(wonkyBr)
+  private val br1: Either[Error, Borrower] = jsonStringToBorrower(jsonStringBr)
+  private val brWonky: Either[Error, Borrower] = jsonStringToBorrower(wonkyBr)
 
   "A Borrower" should "create itself properly - br1" in {
     br1 match {
       case Left(e) => assert(e.toString == "")
-      case Right(br) => assert(Borrower.getName(br) == "Borrower1")
+      case Right(br) => assert(getName(br) == "Borrower1")
     }
     br1 match {
       case Left(e) => assert(e.toString == "")
@@ -21,7 +22,7 @@ class BorrowerSpec extends AnyFlatSpec {
     }
     br1 match {
       case Left(e) => assert(e.toString == "")
-      case Right(br) => assert(Borrower.getMaxBooks(br) == 1)
+      case Right(br) => assert(getMaxBooks(br) == 1)
     }
     br1 match {
       case Left(e) => assert(e.toString == "")
@@ -32,32 +33,32 @@ class BorrowerSpec extends AnyFlatSpec {
   it should "NOT create itself properly - brWonky" in {
     brWonky match {
       case Left(e) => assert(e.toString == "DecodingFailure(Missing required field, List(DownField(name)))")
-      case Right(br) => assert(Borrower.getName(br) == "")
+      case Right(br) => assert(getName(br) == "")
     }
   }
 
   it should "set a new name and maxBooks" in {
     br1 match {
       case Left(e) => assert(e.toString == "")
-      case Right(br) => assert(Borrower.setName("Borrower1", Borrower("Jack", 1)) == br)
+      case Right(br) => assert(setName("Borrower1", Borrower("Jack", 1)) == br)
     }
     br1 match {
       case Left(e) => assert(e.toString == "")
-      case Right(br) => assert(Borrower.setMaxBooks(1, Borrower("Borrower1", 11)) == br)
+      case Right(br) => assert(setMaxBooks(1, Borrower("Borrower1", 11)) == br)
     }
   }
 
   it should "return a string \"Borrower1 (1 books)\"" in {
     br1 match {
       case Left(e) => assert(e.toString == "")
-      case Right(br) => assert(Borrower.borrowerToString(br) == "Borrower1 (1 books)")
+      case Right(br) => assert(borrowerToString(br) == "Borrower1 (1 books)")
     }
   }
 
   it should "turn a Borrower into a JSON string" in {
     br1 match {
       case Left(e) => assert(e.toString == "")
-      case Right(br) => assert(Borrower.borrowerToJsonString(br) == jsonStringBr)
+      case Right(br) => assert(borrowerToJsonString(br) == jsonStringBr)
     }
   }
 }
